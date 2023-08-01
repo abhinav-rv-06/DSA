37b35e4fc69a3702f17eaacbd4d58ff794f8167e
abhinav-rv-06 <108136409+abhinav-rv-06@users.noreply.github.com>
2023-08-01 08:32:19 +0530

    Create minmaxinarray.java

---
+++ arrays/minmaxinarray.java
@@ -0,0 +1,21 @@
+// Find minimum and maximum element in an array
+
+class Compute 
+{
+    static Pair getMinMax(long a[], long n)  
+    {
+        long min = a[0];
+        long max = a[0];
+        
+        for (int i=0; i<n; i++){
+            if(a[i]>max){
+                max = a[i];
+            }
+            if(a[i]<min){
+                min = a[i];
+            }
+        }
+        
+        return new Pair(min,max);
+    }
+}
