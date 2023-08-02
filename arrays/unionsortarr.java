841dfdc1a375a15ebd99cd4c62a1d710159a6615
abhinav-rv-06 <108136409+abhinav-rv-06@users.noreply.github.com>
2023-08-02 07:43:40 +0530

    Create unionsortarr.java

---
+++ arrays/unionsortarr.java
@@ -0,0 +1,39 @@
+// Union of two Sorted Arrays
+
+class Solution
+{
+    public static ArrayList<Integer> findUnion(int arr1[], int arr2[], int n, int m)
+    {
+        int i = 0, j = 0; 
+        ArrayList<Integer> l = new ArrayList<Integer>();
+        while (i < n && j < m) 
+        {
+            while(  i+1<n  &&  arr1[i]==arr1[i+1]  )
+                i++;
+            while(  j+1<m  &&  arr2[j]==arr2[j+1]  )
+                j++;
+            if (arr1[i] < arr2[j]) 
+                l.add(arr1[i++]);
+            else if (arr2[j] < arr1[i]) 
+                l.add(arr2[j++]);
+            else
+            {
+                l.add(arr2[j++]);
+                i++;
+            }
+        }
+        while(i < n)
+        {
+            while(  i+1<n  &&  arr1[i]==arr1[i+1]  )
+                i++;
+            l.add(arr1[i++]);
+        }
+        while(j < m)
+        {
+            while(  j+1<m  &&  arr2[j]==arr2[j+1]  )
+                j++;
+            l.add(arr2[j++]);
+        }
+        return l;   
+    }
+}
