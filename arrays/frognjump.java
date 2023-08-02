40e8d9430d9973c3049a56f57a639010124c17cf
abhinav-rv-06 <108136409+abhinav-rv-06@users.noreply.github.com>
2023-08-02 08:30:38 +0530

    Create frognjump.java

---
+++ arrays/frognjump.java
@@ -0,0 +1,25 @@
+// Frogs and Jumps
+
+//make array for number of leaves
+// for each frog mark next multiple nodes as visited
+//traverse the array and find count of unvisited
+
+class Solution {
+    public int unvisitedLeaves(int N, int leaves, int frogs[]) {
+        int leaveStatus[] = new int[leaves+1]; 
+		for (int i = 0; i < N; i++) {
+			if (frogs[i] <= leaves && leaveStatus[frogs[i]] == 0) {
+				for (int j = frogs[i]; j <= leaves; j += frogs[i]) {
+					leaveStatus[j] = 1;
+				}
+			}
+		}
+		int leafCount = leaves;
+		for (int i : leaveStatus) {
+			if (i==1) {
+				leafCount--;
+			}
+		}
+		return leafCount;
+    }
+}
