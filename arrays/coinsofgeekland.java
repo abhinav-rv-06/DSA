6c08a4f83ea5de8f130d1f341793a8335c43e9ae
abhinav-rv-06 <108136409+abhinav-rv-06@users.noreply.github.com>
2023-08-04 10:10:46 +0530

    Create coinsofgeekland.java

---
+++ arrays/coinsofgeekland.java
@@ -0,0 +1,28 @@
+// Coins of geekland
+
+class Solution
+{
+    public int Maximum_Sum(int mat[][],int N,int K){
+        int mx = Integer.MIN_VALUE;
+        int dp[][] = new int[n+1][n+1];
+        for(int i=0;i<=n;i++){
+            
+            for(int j=0;j<=n;j++){
+                
+                if(i == 0 || j == 0)
+                    dp[i][j] = 0;
+                else{
+                    dp[i][j] = mat[i-1][j-1] + 
+                    dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1]; 
+                   
+                }
+                if(i>=k && j>=k){
+                    mx = Math.max(dp[i][j]-(dp[i][j-k] + 
+                    dp[i-k][j] - dp[i-k][j-k]),mx);
+                    
+                }
+            }
+        }
+        return mx;
+    }
+}
