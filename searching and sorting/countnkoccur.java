75fc8dbbcc9df764f90b626cfdbd1d535c0b8980
abhinav-rv-06 <108136409+abhinav-rv-06@users.noreply.github.com>
2023-08-21 10:06:49 +0530

    Create countnkoccur.java

---
+++ searching and sorting/countnkoccur.java
@@ -0,0 +1,18 @@
+// Count more than n/k occurrences
+
+class Solution 
+{
+    public int countOccurence(int[] arr, int n, int k) 
+    {
+        int barrier=n/k;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int count=0;
+        for(int i=0;i<n;i++){
+            map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+        }
+        for(Map.Entry<Integer,Integer> element:map.entrySet()){
+            if(element.getValue()>barrier) count++;
+        }
+        return count;
+    }
+}
