a5ff72d4418a5e843cc2c5ca28da8c02d97c88b9
abhinav-rv-06 <108136409+abhinav-rv-06@users.noreply.github.com>
2023-10-11 11:54:19 +0530

    Create printlinkedlist.java

---
+++ LinkedList/printlinkedlist.java
@@ -0,0 +1,22 @@
+/* Node is defined as
+class Node {
+    int data;
+    Node next;
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}*/
+
+class Solution
+{
+    void display(Node head)
+    {
+        Node temp = head;
+        while(temp != null)
+        {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+}
